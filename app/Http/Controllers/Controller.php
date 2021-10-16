1fa4932d69ec187ed78673100c77b7ad34d62679
Aleksander <mrhdolek14@gmail.com>
2021-10-16 22:24:33 +0200

    feat: add swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,6 +9,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="E-garage Api",
+ *    version="0.0.1",
+ * )
+ */
+
+/**
+ * @OA\Get(
+ *     path="/docs/v1",
+ *     tags={"documentation"},
+ *     summary="E-garage api",
+ *     @OA\Response(response="200", description="success"),
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests;
