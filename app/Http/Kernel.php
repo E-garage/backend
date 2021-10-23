e595afd384e386ca5ca54ad9e593a1feb7da8de4
Michał Gałęzyka <michal.galezyka@stud.pwsz.legnica.edu.pl>
2021-10-23 19:58:18 +0200

    sanctum_JWT
    configurate sanctum and JWT token

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
@@ -42,8 +43,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            EnsureFrontendRequestsAreStateful::class,
+            'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
