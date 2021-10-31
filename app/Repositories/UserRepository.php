17ff11f155104f0e8a553faaf6e125bb69952877
Grzegorz Bogusz <gbogusz55@gmail.com>
2021-10-31 13:10:20 +0100

    feat(WIT-13): fix types in UserRepository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -5,10 +5,11 @@ declare(strict_types = 1);
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 
 class UserRepository extends BaseRepository
 {
-    protected User $model;
+    protected Model $model;
 
     /**
      * UserRepository constructor.
