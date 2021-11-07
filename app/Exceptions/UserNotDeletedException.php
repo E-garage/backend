0ae3217b4c51940046742e3a49bd957d2147e3f6
Grzegorz Bogusz <gbogusz55@gmail.com>
2021-11-07 19:28:21 +0100

    feat(WIT-13): created custom exceptions

---
+++ app/Exceptions/UserNotDeletedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class UserNotDeletedException extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct("Couldn\'t find or delete the user.", 500);
+    }
+}
---
+++ app/Exceptions/UserNotUpdatedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class UserNotUpdatedException extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct("Couldn\'t find or update the user.", 500);
+    }
+}
