ce975886f2b97fc9f3b30d209502177b45a7f6c0
Grzegorz Bogusz <gbogusz55@gmail.com>
2021-12-14 07:07:12 +0100

    Feat(WIT-16): added swagger annotations

--- app/Http/Controllers/User/AccountManagementController.php
+++ app/Http/Controllers/User/AccountManagementController.php
@@ -11,6 +11,70 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * @OA\Put(
+ *     path="/api/v1/account/update/password",
+ *     tags={"Account Management"},
+ *     summary="Update user's account password",
+ *     @OA\Parameter(
+ *         parameter="user_credentials_in_query_required",
+ *         name="body",
+ *         in="query",
+ *         required=true,
+ *         description="Data needed to perform action.",
+ *         @OA\Schema(ref="#/components/schemas/UpdatePassword"),
+ *     ),
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/UpdatePassword"),
+ *         ),
+ *     ),
+ *     @OA\Response(response="200", description=""),
+ * ),
+ *
+ * @OA\Put(
+ *     path="/api/v1/account/update/name",
+ *     tags={"Account Management"},
+ *     summary="Update user's account name",
+ *     @OA\Parameter(
+ *         parameter="user_credentials_in_query_required",
+ *         name="body",
+ *         in="query",
+ *         required=true,
+ *         description="Data needed to perform action.",
+ *         @OA\Schema(ref="#/components/schemas/UpdateName"),
+ *     ),
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/UpdateName"),
+ *         ),
+ *     ),
+ *     @OA\Response(response="200", description=""),
+ * ),
+ *
+ * @OA\Put(
+ *     path="/api/v1/account/update/email",
+ *     tags={"Account Management"},
+ *     summary="Update user's account email",
+ *     @OA\Parameter(
+ *         parameter="user_credentials_in_query_required",
+ *         name="body",
+ *         in="query",
+ *         required=true,
+ *         description="Data needed to perform action.",
+ *         @OA\Schema(ref="#/components/schemas/UpdateEmail"),
+ *     ),
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/UpdateEmail"),
+ *         ),
+ *     ),
+ *     @OA\Response(response="200", description=""),
+ * )
+ */
 class AccountManagementController extends Controller
 {
     protected UserModel $user;
@@ -22,6 +86,22 @@ class AccountManagementController extends Controller
         $this->service = new UserAccountManagementService($this->user);
     }
 
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="UpdatePassword",
+     *             type="object",
+     *         @OA\Property(
+     *             property="password",
+     *             type="string"
+     *         ),
+     *         @OA\Property(
+     *             property="password_confirmation",
+     *             type="string"
+     *         ),
+     *         example={"password": "12345678", "password_confirmation": "12345678"}
+     * )
+     */
     public function updatePassword(Request $request): JsonResponse
     {
         $newPassword = (string)$request['password'];
@@ -30,6 +110,18 @@ class AccountManagementController extends Controller
         return new JsonResponse();
     }
 
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="UpdateEmail",
+     *             type="object",
+     *         @OA\Property(
+     *             property="email",
+     *             type="email|string"
+     *         ),
+     *         example={"email": "test@test.com"}
+     * )
+     */
     public function updateEmail(Request $request): JsonResponse
     {
         $newEmail = (string)$request['email'];
@@ -38,6 +130,18 @@ class AccountManagementController extends Controller
         return new JsonResponse();
     }
 
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="UpdateName",
+     *             type="object",
+     *         @OA\Property(
+     *             property="name",
+     *             type="string"
+     *         ),
+     *         example={"name": "JohnDoe"}
+     * )
+     */
     public function updateName(Request $request): JsonResponse
     {
         $newName = (string)$request['name'];
