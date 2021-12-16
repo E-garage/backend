cb649b4e654e08b797282fcddc64f19b131eca47
Grzegorz Bogusz <gbogusz55@gmail.com>
2021-12-16 14:05:00 +0100

    Feat(WIT-16): added tests for deleting and getting avatar

--- tests/Feature/AvatarManagementTest.php
+++ tests/Feature/AvatarManagementTest.php
@@ -8,11 +8,10 @@ use Illuminate\Http\UploadedFile;
 use Storage;
 use Tests\TestCase;
 
-class AvatarUploadTest extends TestCase
+class AvatarManagementTest extends TestCase
 {
     use RefreshDatabase;
 
-    protected string $uri = '/api/v1/account/upload-avatar';
     protected UserModel $user;
 
     protected function setUp(): void
@@ -26,7 +25,7 @@ class AvatarUploadTest extends TestCase
     public function testAvatarUplodedSuccessfully()
     {
         $file = UploadedFile::fake()->image('test.png');
-        $response = $this->postJson($this->uri, ['image' => $file]);
+        $response = $this->postJson('/api/v1/account/avatar/upload', ['image' => $file]);
 
         $response->assertOk();
         Storage::disk('user_avatars')->assertExists($file->hashName());
@@ -35,7 +34,7 @@ class AvatarUploadTest extends TestCase
     public function testFilenameWasPersistedToDatabase()
     {
         $file = UploadedFile::fake()->image('test.png');
-        $response = $this->postJson($this->uri, ['image' => $file]);
+        $this->postJson('/api/v1/account/avatar/upload', ['image' => $file]);
         $filename = $this->user['avatar'];
 
         $this->assertNotNull($filename);
@@ -44,35 +43,67 @@ class AvatarUploadTest extends TestCase
     public function testPersistedFilenameAndOriginalOneAreTheSame()
     {
         $file = UploadedFile::fake()->image('test.png');
-        $response = $this->postJson($this->uri, ['image' => $file]);
+        $this->postJson('/api/v1/account/avatar/upload', ['image' => $file]);
         $filenameFromDatabase = $this->user['avatar'];
         $filename = $file->hashName();
 
         $this->assertSame($filename, $filenameFromDatabase);
     }
 
+    public function testAvatarWasRetrievedFromStorage()
+    {
+        $file = UploadedFile::fake()->image('test.png');
+        $this->postJson('/api/v1/account/avatar/upload', ['image' => $file]);
+
+        $filename = $this->user['avatar'];
+        $response = $this->getJson('/api/v1/account/avatar/');
+
+        $response->assertOk();
+        $response->assertDownload($filename);
+    }
+
+    public function testAvatarWasDeletedFromTheStorage()
+    {
+        $file = UploadedFile::fake()->image('test.png');
+        $this->postJson('/api/v1/account/avatar/upload', ['image' => $file]);
+
+        $filename = $this->user['avatar'];
+        $response = $this->deleteJson('/api/v1/account/avatar/delete');
+
+        $this->assertNotNull($filename);
+        $response->assertOk();
+        Storage::disk('user_avatars')->assertMissing($filename);
+    }
+
+    public function testFilenameWasDeletedFromDatabase()
+    {
+        $filename = $this->user['avatar'];
+
+        $this->assertNull($filename);
+    }
+
     public function testValidationExceptionHasOccured()
     {
         //empty request
-        $response = $this->postJson($this->uri);
+        $response = $this->postJson('/api/v1/account/avatar/upload');
         $response->assertUnprocessable();
 
         //request with string
-        $response = $this->postJson($this->uri, ['image' => 'text-instead-of-image']);
+        $response = $this->postJson('/api/v1/account/avatar/upload', ['image' => 'text-instead-of-image']);
         $response->assertUnprocessable();
 
         //request with integer
-        $response = $this->postJson($this->uri, ['image' => 12345]);
+        $response = $this->postJson('/api/v1/account/avatar/upload', ['image' => 12345]);
         $response->assertUnprocessable();
 
         //request with pdf file
         $file = UploadedFile::fake()->create('fake', 0, 'pdf');
-        $response = $this->postJson($this->uri, ['image' => $file]);
+        $response = $this->postJson('/api/v1/account/avatar/upload', ['image' => $file]);
         $response->assertUnprocessable();
 
         //request with svg file
         $file = UploadedFile::fake()->create('fake', 0, 'svg');
-        $response = $this->postJson($this->uri, ['image' => $file]);
+        $response = $this->postJson('/api/v1/account/avatar/upload', ['image' => $file]);
         $response->assertUnprocessable();
     }
 
