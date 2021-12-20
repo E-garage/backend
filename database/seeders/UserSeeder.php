e39a907a43bc16f8017aa6ff9dd8d03b6df370dc
Grzegorz Bogusz <gbogusz55@gmail.com>
2021-12-20 09:34:48 +0100

    Feat(WIT-17): added user's seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserModel;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createAdmin();
+    }
+
+    private function createAdmin()
+    {
+        UserModel::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@egarage.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'role_id' => 1,
+        ]);
+    }
+}
