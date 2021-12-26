4fcc4699fadb214260dadb20a616370d422310cb
Grzegorz Bogusz <gbogusz55@gmail.com>
2021-12-26 11:01:24 +0100

    Feat(WIT-63): added swagger annotations

--- app/Http/Controllers/User/ResetPasswordController.php
+++ app/Http/Controllers/User/ResetPasswordController.php
@@ -10,8 +10,63 @@ use App\Services\SendResetPasswordLinkService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\PUT(
+ *     path="/api/v1/reset-password/send-link",
+ *     tags={"Reset Password"},
+ *     summary="Send link to user's mail via mail.",
+ *     @OA\Parameter(
+ *         parameter="user_credentials_in_query_required",
+ *         name="body",
+ *         in="query",
+ *         required=true,
+ *         description="Email to which the link should be sent.",
+ *         @OA\Schema(ref="#/components/schemas/SendResetPasswordLink"),
+ *     ),
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/SendResetPasswordLink"),
+ *         ),
+ *     ),
+ *     @OA\Response(response="200", description=""),
+ * ),
+ *
+ * @OA\PUT(
+ *     path="/api/v1/reset-password/",
+ *     tags={"Reset Password"},
+ *     summary="Resets password.",
+ *     @OA\Parameter(
+ *         parameter="user_credentials_in_query_required",
+ *         name="body",
+ *         in="query",
+ *         required=true,
+ *         description="User's email, unique token and new password.",
+ *         @OA\Schema(ref="#/components/schemas/ResetPassword"),
+ *     ),
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/ResetPassword"),
+ *         ),
+ *     ),
+ *     @OA\Response(response="200", description=""),
+ * ),
+ */
 class ResetPasswordController extends Controller
 {
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="SendResetPasswordLink",
+     *             type="object",
+     *         @OA\Property(
+     *             property="email",
+     *             type="email|string"
+     *         ),
+     *         example={"email": "cool@email.com"}),
+     * )
+     */
     public function sendResetLink(Request $request): JsonResponse
     {
         $email = $request->only('email');
@@ -22,9 +77,38 @@ class ResetPasswordController extends Controller
             throw new ResetLinkNotSentException();
         }
 
-        return new JsonResponse(['link_sent' => $isSent]);
+        return new JsonResponse();
     }
 
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="ResetPassword",
+     *             type="object",
+     *         @OA\Property(
+     *             property="token",
+     *             type="string",
+     *         ),
+     *         @OA\Property(
+     *             property="email",
+     *             type="email|string",
+     *         ),
+     *         @OA\Property(
+     *             property="password",
+     *             type="string",
+     *         ),
+     *         @OA\Property(
+     *             property="password_confirmation",
+     *             type="string",
+     *         ),
+     *         example={
+     *             "token": "cdacc0d252881102003fb486b419f2de52d8f7b1779585342e5b04c2003ce10d",
+     *             "email": "cool@email.com",
+     *             "password": "12345678",
+     *             "password_confirmation": "12345678",
+     *         }),
+     * )
+     */
     public function resetPassword(Request $request): JsonResponse
     {
         $data = $this->getDataFromRequest($request);
@@ -36,7 +120,7 @@ class ResetPasswordController extends Controller
             throw new PasswordNotResetedException();
         }
 
-        return new JsonResponse(['password_reseted' => $isReseted]);
+        return new JsonResponse();
     }
 
     private function getDataFromRequest(Request $request): array
