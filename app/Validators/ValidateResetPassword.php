d8632bd6bfef1933814ed185288b28123daba627
Grzegorz Bogusz <gbogusz55@gmail.com>
2021-12-29 18:08:14 +0100

    Fix(WIT-63): middleware is now completed

--- app/Validators/ValidateResetPassword.php
+++ app/Validators/ValidateResetPassword.php
@@ -1,9 +1,13 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Validators;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Validator;
 
 class ValidateResetPassword
 {
@@ -14,7 +18,7 @@ class ValidateResetPassword
      *
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): mixed
     {
         $rules = [
             'token' => 'required',
@@ -22,6 +26,12 @@ class ValidateResetPassword
             'password' => 'required|min:8|max:50|confirmed',
             'password_confirmation' => 'min:8|max:50',
         ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+            throw new ValidationException($validator);
+        }
 
         return $next($request);
     }
