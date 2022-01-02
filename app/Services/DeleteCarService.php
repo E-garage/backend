c19b6f700dc0cb5d24b891fdc0ba56904a4491ad
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-02 16:06:34 +0100

    Refactor(WIT-23): separated logic for removing car's thumbnails

--- app/Services/DeleteCarService.php
+++ app/Services/DeleteCarService.php
@@ -4,47 +4,27 @@ declare(strict_types = 1);
 
 namespace App\Services;
 
-use App\Exceptions\CarsThumbnailNotRemovedFromStorage;
 use App\Models\Car;
 use App\Repositories\CarRepository;
-use Storage;
 
 class DeleteCarService
 {
     protected Car $car;
     protected CarRepository $repository;
+    protected CarThumbnailDeletionService $service;
 
     public function __construct(Car $car)
     {
         $this->car = $car;
         $this->repository = new CarRepository($this->car);
+        $this->service = new CarThumbnailDeletionService();
     }
 
     public function deleteCar(): void
     {
-        $this->repository->delete();
-        $this->removeThumbnailFromStorage();
-
-        return;
-    }
-
-    /**
-     * Removes car's thumbnail from storage if exists.
-     *
-     * @throws CarsThumbnailNotRemovedFromStorage
-     */
-    private function removeThumbnailFromStorage(): void
-    {
         $filename = $this->car['thumbnail'];
 
-        if (!$filename) {
-            return;
-        }
-
-        $success = Storage::disk('cars_thumbnails')->delete($filename);
-
-        if (!$success) {
-            throw new CarsThumbnailNotRemovedFromStorage();
-        }
+        $this->repository->delete();
+        $this->service->deleteThumbnail($filename);
     }
 }
