746067e58db4b84b45557a903c620ae7aa77a914
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-04 18:44:08 +0100

    Feat(WIT-24): added custom event listener

---
+++ app/Listeners/CreateLastParkedLocation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\LastParkedLocation;
+use Illuminate\Auth\Events\Registered;
+
+class CreateLastParkedLocation
+{
+    /**
+     * Handle the event.
+     *
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        if (!$event->user->location) { //@phpstan-ignore-line
+            LastParkedLocation::create([
+                'user_id' => $event->user['id'],
+            ]);
+        }
+    }
+}
