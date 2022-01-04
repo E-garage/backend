29b8bde435ade429e733f0137a4dcf6b060f5144
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-04 18:41:17 +0100

    Feat(WIT-24): added middleware for validation

---
+++ app/Validators/ValidateSetLastParkedLocation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Validators;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Validator;
+
+class ValidateSetLastParkedLocation
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     *
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $rules = [
+            'longitude' => 'numeric|required|regex:/^(\+|-)(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$',
+            'latitude' => 'numeric|required|regex:/^(\+|-)(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        return $next($request);
+    }
+}
