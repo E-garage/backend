03abccb286d826e9637e9c2456dfe698c35884bf
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-05 14:45:45 +0100

    Feat(WIT-24): added logic for deleting location

--- app/Services/LastParkedLocationService.php
+++ app/Services/LastParkedLocationService.php
@@ -16,17 +16,37 @@ class LastParkedLocationService
         $this->repository = new LastParkedLocationRepository();
     }
 
+    /**
+     * @throws \App\Exceptions\LastParkedLocationNotUpdatedException
+     */
     public function setLocation($coordinates): void
     {
         $user = Auth::user();
         $this->repository->update($user, $coordinates); //@phpstan-ignore-line
     }
 
+    /**
+     * @throws \App\Exceptions\LastParkedLocationNotRetrievedException
+     */
     public function getLocation(): array
     {
         $user = Auth::user();
-        $coordinates = $this->repository->get($user); //@phpstan-ignore-line
 
-        return $coordinates;
+        return $this->repository->get($user); //@phpstan-ignore-line
+    }
+
+    /**
+     * @throws \App\Exceptions\LastParkedLocationNotUpdatedException
+     */
+    public function deleteLocation(): void
+    {
+        $user = Auth::user();
+
+        $coordinates = [
+            'longitude' => null,
+            'latitude' => null,
+        ];
+
+        $this->repository->update($user, $coordinates); //@phpstan-ignore-line
     }
 }
