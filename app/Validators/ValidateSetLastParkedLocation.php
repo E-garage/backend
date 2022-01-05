df41efeb84f7b37d5b3b9bc11ea1daff8d86f851
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-05 14:46:03 +0100

    Fix(WIT-24): fixed rules

--- app/Validators/ValidateSetLastParkedLocation.php
+++ app/Validators/ValidateSetLastParkedLocation.php
@@ -1,9 +1,13 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Validators;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Validator;
 
@@ -12,15 +16,17 @@ class ValidateSetLastParkedLocation
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      *
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @throws ValidationException
+     *
+     * @return Response|RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): mixed
     {
         $rules = [
-            'longitude' => 'numeric|required|regex:/^(\+|-)(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$',
-            'latitude' => 'numeric|required|regex:/^(\+|-)(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/',
+            'longitude' => ['numeric', 'required', 'regex:/^(\+|-)(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$/'],
+            'latitude' => ['numeric', 'required', 'regex:/^(\+|-)(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/'],
         ];
 
         $validator = Validator::make($request->all(), $rules);
