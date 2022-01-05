5e9f27bebef8d5f16c38757020b8cf1bd8750ab7
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-05 14:41:28 +0100

    Feat(WIT-24): added delete method and swagger annotations

--- app/Http/Controllers/LastParkedLocationController.php
+++ app/Http/Controllers/LastParkedLocationController.php
@@ -8,6 +8,43 @@ use App\Services\LastParkedLocationService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\POST(
+ *     path="/api/v1/last-parked-location/set",
+ *     tags={"Last parked location management"},
+ *     summary="Set coordinates.",
+ *     @OA\Parameter(
+ *         parameter="user_credentials_in_query_required",
+ *         name="body",
+ *         in="query",
+ *         required=true,
+ *         description="Coordinates of last parked location.",
+ *         @OA\Schema(ref="#/components/schemas/Coordinates"),
+ *     ),
+ *     @OA\Response(response="200", description="Success"),
+ * ),
+ *
+ * @OA\GET(
+ *     path="/api/v1/last-parked-location/",
+ *     tags={"Last parked location management"},
+ *     summary="Get coordinates.",
+ *     @OA\Response(
+ *          response="200",
+ *          description="Success",
+ *          @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/Coordinates"),
+ *         ),
+ *     ),
+ * ),
+ *
+ * @OA\DELETE(
+ *     path="/api/v1/last-parked-location/delete",
+ *     tags={"Last parked location management"},
+ *     summary="Delete coordinates.",
+ *     @OA\Response(response="200", description="Success"),
+ * ),
+ */
 class LastParkedLocationController extends Controller
 {
     protected LastParkedLocationService $service;
@@ -17,6 +54,25 @@ class LastParkedLocationController extends Controller
         $this->service = new LastParkedLocationService();
     }
 
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="Coordinates",
+     *             type="object",
+     *         @OA\Property(
+     *             property="longitude",
+     *             type="numeric|required"
+     *         ),
+     *         @OA\Property(
+     *             property="latitude",
+     *             type="numeric|required"
+     *         ),
+     *         example={
+     *              "longitude": "+50.235423",
+     *              "latitude": "-15.152354",
+     *         },
+     * )
+     */
     public function set(Request $request): JsonResponse
     {
         $data = $request->only(['longitude', 'latitude']);
@@ -30,5 +86,12 @@ class LastParkedLocationController extends Controller
         $coordinates = $this->service->getLocation();
 
         return new JsonResponse($coordinates);
+    }
+
+    public function delete(): JsonResponse
+    {
+        $this->service->deleteLocation();
+
+        return new JsonResponse();
     }
 }
