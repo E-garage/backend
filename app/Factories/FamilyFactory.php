b8235af7d68946ebe6092d67bd3ab7e94faddf59
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-09 22:08:21 +0100

    Feat(WIT-22): added factory

---
+++ app/Factories/FamilyFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Factories;
+
+use App\Models\Family;
+use Auth;
+
+class FamilyFactory
+{
+    private Family $family;
+
+    public function __construct()
+    {
+        $this->family = new Family();
+    }
+
+    public function createFormRequest(array $data): Family
+    {
+        $this->family->owner_id = Auth::user();
+        $this->family->name = $data['name'];
+        $this->family->description = $data['description'];
+
+        return $this->family;
+    }
+}
