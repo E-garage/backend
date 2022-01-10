6c6ed462f7304a368b47d71ab7695d38f71ff8b8
Aleksander <mrhdolek14@gmail.com>
2022-01-10 02:05:22 +0100

    Feat(ak): modyfi ci (#59)
    * feat: update ci
    
    * fix: ci
    
    * test: kubectl
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * feat: update ci
    
    * fix: providers
    
    * fix: delete tmp
    
    * fix: return ci
    
    * fix: check mails
    
    * fix: dekete ci
    
    * Revert "fix: check mails"
    
    This reverts commit ab3b7926d0a678248fda602969ba1c1012ed77e6.
    
    * fix: repo
    
    * Revert "fix: dekete ci"
    
    This reverts commit 3c6d8c76d4a89e35f3c4fa946b524223102d089c.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ use App\Models\UserModel;
 use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,6 +30,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
         UserModel::observe(UserObserver::class);
     }
 }
