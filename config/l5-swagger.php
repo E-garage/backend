804289c630f5bda48805dd235b4d2418be29858e
Aleksander <mrhdolek14@gmail.com>
2022-01-11 22:53:26 +0100

    fix: revert

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -99,7 +99,7 @@ return [
             /*
              * Edit to include full URL in ui for assets
             */
-            'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', false),
+            'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
 
             /*
              * Absolute path to directories that should be exclude from scanning
