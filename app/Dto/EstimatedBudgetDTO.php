bbed94bebc134f8e5d5880b5c252941a2b54b875
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-14 22:59:06 +0100

    Feat(WIT-32): added DTO

---
+++ app/Dto/EstimatedBudgetDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Dto;
+
+use App\Exceptions\MappingDataToObjectException;
+use App\Models\EstimatedBudget;
+
+class EstimatedBudgetDTO
+{
+    public function mapDataToObject(array $data, EstimatedBudget $budget): EstimatedBudget
+    {
+        try {
+            foreach($data as $key => $value)
+            {
+                $budget[$key] = $data[$key];
+            }
+
+            return $budget;
+        } catch (\Throwable) {
+            throw new MappingDataToObjectException();
+        }
+    }
+}
