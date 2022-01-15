845f8bf96b272f7f281d7f5ace03b262cd8b4527
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-15 04:20:03 +0100

    Feat(WIT-32): added swagger annotations

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\BudgetNotDeletedException;
+use App\Exceptions\BudgetNotUpdatedException;
+use App\Exceptions\CarBudgetNotFoundException;
 use App\Models\EstimatedBudget;
 use App\Services\DeleteBudgetService;
 use App\Services\RetrieveBudgetService;
@@ -10,8 +13,101 @@ use Auth;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * * @OA\GET(
+ *     path="/api/v1/car-budget/{budget_id}/",
+ *     tags={"Estimated Budget Management"},
+ *     security={{"bearerAuth": {}}},
+ *     summary="Get estimated budget for given car.",
+ *     @OA\Response(
+ *          response="200",
+ *          description="Success",
+ *          @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 ref="#/components/schemas/Budget",
+ *             ),
+ *         ),
+ *     ),
+ *     @OA\Response(response="500", description="Couldnt find estimated budget for the car."),
+ * ),
+ *
+ * @OA\PUT(
+ *     path="/api/v1/car-budget/{budget_id}/update/original-budget",
+ *     tags={"Estimated Budget Management"},
+ *     security={{"bearerAuth": {}}},
+ *     summary="Updated estimated original budget for car.",
+ *     @OA\Parameter(
+ *     name="budget_id",
+ *          required=true,
+ *          in="path",
+ *          @OA\Schema(type="integer")
+ *     ),
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/UpdateOriginalBudget"),
+ *         ),
+ *     ),
+ *     @OA\Response(response="200", description="Success update budget"),
+ *     @OA\Response(response="422", description="Couldn't update car's budget."),
+ * ),
+ *
+ * @OA\PUT(
+ *     path="/api/v1/car-budget/{budget_id}/update/last-payment",
+ *     tags={"Estimated Budget Management"},
+ *     security={{"bearerAuth": {}}},
+ *     summary="Updated last payment amount.",
+ *     @OA\Parameter(
+ *     name="budget_id",
+ *          required=true,
+ *          in="path",
+ *          @OA\Schema(type="integer")
+ *     ),
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/UpdateLastPayment"),
+ *         ),
+ *     ),
+ *     @OA\Response(response="200", description="Success update budget"),
+ *     @OA\Response(response="422", description="Couldn't update car's budget."),
+ * ),
+ *
+ * @OA\DELETE(
+ *     path="/api/v1/car-budget/{budget_id}/delete",
+ *     tags={"Estimated Budget Management"},
+ *     security={{"bearerAuth": {}}},
+ *     summary="Delete budget.",
+ *     @OA\Parameter(
+ *          name="budget_id",
+ *          required=true,
+ *          in="path",
+ *          @OA\Schema(type="integer")
+ *     ),
+ *     @OA\Response(response="200", description="Success delete budget"),
+ *     @OA\Response(response="500", description="Success delete budget"),
+ * ),
+ */
 class BudgetController extends Controller
 {
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="UpdateOriginalBudget",
+     *             type="object",
+     *         @OA\Property(
+     *             property="original_budget",
+     *             type="numeric|required"
+     *         ),
+     *         example={
+     *              "original_budget": "130",
+     *         },
+     * )
+     */
+    /**
+     * @throws BudgetNotUpdatedException
+     */
     public function updateOriginalBudget(Request $request, EstimatedBudget $budget): JsonResponse
     {
         if (Auth::user()->cannot('update', $budget)) {
@@ -26,6 +122,23 @@ class BudgetController extends Controller
         return new JsonResponse();
     }
 
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="UpdateLastPayment",
+     *             type="object",
+     *         @OA\Property(
+     *             property="last_payment_amount",
+     *             type="numeric|required"
+     *         ),
+     *         example={
+     *              "last_payment_amount": "130",
+     *         },
+     * )
+     */
+    /**
+     * @throws BudgetNotUpdatedException
+     */
     public function updateLastPayment(Request $request, EstimatedBudget $budget): JsonResponse
     {
         if (Auth::user()->cannot('update', $budget)) {
@@ -40,6 +153,43 @@ class BudgetController extends Controller
         return new JsonResponse();
     }
 
+    /**
+     * @OA\Component(
+     *         @OA\Schema(
+     *             schema="Budget",
+     *             type="object",
+     *         @OA\Property(
+     *             property="last_payment_amount",
+     *             type="numeric|required"
+     *         ),
+     *         example={
+     *              "budget": {
+     *              "id": 2,
+     *              "car_id": 2,
+     *              "original_budget": "0",
+     *              "budget_left": "0",
+     *              "last_payment_amount": null,
+     *              "created_at": "2022-01-15T03:09:07.000000Z",
+     *              "updated_at": "2022-01-15T03:09:07.000000Z",
+     *              "car": {
+     *                  "id": 2,
+     *                  "owner_id": "fd31be72-6928-42e2-b2d4-03985219d77a",
+     *                  "brand": "AAA123",
+     *                  "description": "Lorem Ipsum",
+     *                  "thumbnail": null,
+     *                  "created_at": "2022-01-15T03:09:07.000000Z",
+     *                  "updated_at": "2022-01-15T03:09:07.000000Z",
+     *                  "family_id": null,
+     *                  "availability": "available",
+     *                  "details": null
+     *                  }
+     *              }
+     *         },
+     * )
+     */
+    /**
+     * @throws CarBudgetNotFoundException
+     */
     public function get(EstimatedBudget $budget): JsonResponse
     {
         if (Auth::user()->cannot('view', $budget)) {
@@ -52,6 +202,9 @@ class BudgetController extends Controller
         return new JsonResponse(['budget' => $budget]);
     }
 
+    /**
+     * @throws BudgetNotDeletedException
+     */
     public function delete(EstimatedBudget $budget): JsonResponse
     {
         if (Auth::user()->cannot('delete', $budget)) {
