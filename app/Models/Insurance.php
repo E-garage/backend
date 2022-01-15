cba3a01ad12ca836b82992a73b18f5adecb94051
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-01-15 18:28:52 +0100

    Feat(WIT-30): completed models

--- app/Models/Insurance.php
+++ app/Models/Insurance.php
@@ -2,13 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Insurance extends Model
 {
-    use HasFactory;
+    protected $table = 'insurances';
+
+    protected $fillable = [
+        'car_id',
+        'end_date',
+    ];
+
+    protected $casts = [
+        'end_date' => 'date',
+    ];
 
     public function car(): BelongsTo
     {
